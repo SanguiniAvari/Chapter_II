1b265f8615bbe7ff2afe7fba45f9c62cb30b94b2
SanguiniAvari <volk-post@yandex.ru>
2023-03-23 00:39:41 +0500

    LR_10

--- src/LR_10/homework/LR_10_var_4_4_excel.java
+++ src/LR_10/homework/LR_10_var_4_4_excel.java
@@ -1,5 +1,6 @@
 package LR_10.homework;
 
+import org.apache.poi.EmptyFileException;
 import org.apache.poi.openxml4j.exceptions.NotOfficeXmlFileException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -7,12 +8,14 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import java.util.Scanner;
 
 public class LR_10_var_4_4_excel {
     public static void Miracle(){
-        String filePath = "src/LR_10/homework/lr10_excel.xlsx";
+        String filePath = "src/LR_10/homework/lr10_excel.xsx";
         try(FileInputStream inputStream = new FileInputStream(filePath)) {
             //создаем экземпляр книги эксель из файла
             XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
@@ -29,7 +32,17 @@ public class LR_10_var_4_4_excel {
                 }
                 System.out.println();
             }
-        }catch (NotOfficeXmlFileException e){
+        }catch (FileNotFoundException e){
+            System.out.println("Файл не найден");
+            System.out.println("Для повторной попытки введите \"1\"");
+            System.out.print(">>   ");
+            Scanner scanner = new Scanner(System.in);
+            int bobo = scanner.nextInt();
+            if (bobo == 1) {
+                Miracle();
+            }
+        }
+        catch (NotOfficeXmlFileException e){
             System.out.println("Неправильный формат файла");
             System.out.println("Для повторной попытки введите \"1\"");
             System.out.print(">>   ");
@@ -38,7 +51,28 @@ public class LR_10_var_4_4_excel {
             if (bobo == 1){
                Miracle();
             }
-        }catch (LinkageError e){
+        }
+            catch (NoSuchFileException e){
+                System.out.println("Неправильный формат файла");
+                System.out.println("Для повторной попытки введите \"1\"");
+                System.out.print(">>   ");
+                Scanner scanner = new Scanner(System.in);
+                int bobo = scanner.nextInt();
+                if (bobo == 1){
+                    Miracle();
+                }
+            }
+        catch (EmptyFileException e){
+            System.out.println("Файл пустой");
+            System.out.println("Для повторной попытки введите \"1\"");
+            System.out.print(">>   ");
+            Scanner scanner = new Scanner(System.in);
+            int bobo = scanner.nextInt();
+            if (bobo == 1){
+                Miracle();
+            }
+        }
+        catch (LinkageError e){
             System.out.println("Запрашиваемый лист не найден");
             System.out.println("Для повторной попытки введите \"1\"");
             System.out.print(">>   ");
